a174b954cb1248602655a934781f40b654f0bc4e
Ramliw <Ramliw@Ramliw-PC>
2019-09-08 21:56:18 -0500

    Actulize perfil supervisor un poco

--- src/java/sipad/sena/DTO/NewServlet.java
+++ src/java/sipad/sena/DTO/NewServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sipad.sena.DTO;
 
 import java.io.IOException;
@@ -13,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ASUS
- */
+
 @WebServlet(name = "NewServlet", urlPatterns = {"/NewServlet"})
 public class NewServlet extends HttpServlet {
 
