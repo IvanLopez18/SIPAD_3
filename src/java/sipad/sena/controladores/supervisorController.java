b8c49c7950fec3e24165cc8b600c40702de01f69
Ramliw <Ramliw@Ramliw-PC>
2019-09-14 20:37:32 -0500

    Sigo poniendo cosas al rol de supervisor

--- src/java/sipad/sena/controladores/supervisorController.java
+++ src/java/sipad/sena/controladores/supervisorController.java
@@ -14,11 +14,7 @@ public class supervisorController {
     //Metodo de iniciarsesion
     public String iniciarSession() {
 
-        HttpSession sesion = null;
-
-        sesion.setAttribute("usuario", "Aldemar Rodriguez");
-
-        return "app/supervisor/indexSupervisor";
+        return "app/supervisor/indexSup";
 
     }
 
